a6fca48e61ef86363147a0d2b7de075fbb8e93e6
Kajanth Kumar <kajanthkumar@gmail.com>
2020-04-14 16:27:19 -0400

    added a ajax call for movies

--- scripts.js
+++ scripts.js
@@ -1,3 +1,40 @@
-const quaranscene = {}
+const quaranscene = {};
 
-const quaranscene.apiKey = 'ffb95a5b116cb8ae246c7c6f51c94ed6'
+//name space variables
+quaranscene.apiKey = 'ffb95a5b116cb8ae246c7c6f51c94ed6'; //the apiKey
+
+quaranscene.baseURL = `https://api.themoviedb.org/3/discover/movie`;
+
+quaranscene.genre =27;
+
+quaranscene.movieRequest = () => {
+
+    $.ajax({
+      url: quaranscene.baseURL,
+      method: "GET",
+      dataType: "json",
+      data: {
+        api_key: quaranscene.apiKey,
+        language: "en-US",
+        include_adult: false,
+        with_genres: `${quaranscene.genre}`,
+      },
+    }).then(function (result) {
+      console.log(result);
+    });
+
+}
+
+
+
+
+// Start app
+quaranscene.init = function() {
+    quaranscene.movieRequest();
+};
+
+$(function() {
+    quaranscene.init();
+});
+
+
