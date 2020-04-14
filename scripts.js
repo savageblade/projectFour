257c9714026d4e3ca3719b7619f30804e1f17d87
Lacey Savage <lacey.savage@gmail.com>
2020-04-14 17:57:24 -0400

    Add scss files, genre array, basic HTML

--- scripts.js
+++ scripts.js
@@ -5,7 +5,29 @@ quaranscene.apiKey = 'ffb95a5b116cb8ae246c7c6f51c94ed6'; //the apiKey
 
 quaranscene.baseURL = `https://api.themoviedb.org/3/discover/movie`;
 
-quaranscene.genre =27;
+quaranscene.genreOptions = {
+  action: 28,
+  adventure: 12,
+  animation: 16,
+  comedy: 35,
+  crime: 80,
+  documentary: 99,
+  drama: 18,
+  family: 10751,
+  fantasy: 14,
+  history: 36,
+  horror: 27,
+  music: 10402,
+  mystery: 9648,
+  romance: 10749,
+  scienceFiction: 878,
+  tvMovie: 10770,
+  thriller: 53,
+  war: 10752,
+  western: 37
+}
+
+quaranscene.genre = quaranscene.genreOptions;
 
 quaranscene.movieRequest = () => {
 
