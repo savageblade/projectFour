eba12a8b0c5d5cea3542dfe8e740252f02684706
Lacey Savage <lacey.savage@gmail.com>
2020-04-21 14:35:30 -0400

    Add 2nd js mq point to introBell

--- scripts.js
+++ scripts.js
@@ -331,12 +331,18 @@ quaranscene.init = function() {
     quaranscene.movieRequest(quaranscene.genre);
     // quaranscene.displayMovieResult(quaranscene.movieList);
     $(".introBell").on("click", function() {
-      $("#ring")[0].volume = 0.15;
-      $("#ring")[0].play();
-      if ((window.innerWidth <= 950)) {
+      // $("#ring")[0].volume = 0.15;
+      // $("#ring")[0].play();
+      if ((window.innerWidth <= 750)) {
         document.getElementById("genreSelection").focus({ preventScroll: false });
         window.scrollTo({
-        top: 900,
+        top: 650,
+        behavior: "smooth"
+      })
+    } else if ((window.innerWidth <= 1000)) {
+        document.getElementById("genreSelection").focus({ preventScroll: false });
+        window.scrollTo({
+        top: 650,
         behavior: "smooth"
       })
     } else {
